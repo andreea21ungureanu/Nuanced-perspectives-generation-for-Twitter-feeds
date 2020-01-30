bb973214526e2fc2c702ce24fedacb0e429fe3e0
andreea21ungureanu <andreea.ungureanu221@gmail.com>
2020-01-30 13:29:19 +0000

    Story 4: Create centroids

--- clustering.py
+++ clustering.py
@@ -41,12 +41,42 @@ def divisive_hierarhical_clustering(tweets, nr_of_clusters=3):
 
     return result
 
+def create_centroids(tweets):
+    centroids_dict = {}
+    cluster_memeber_count = {}
+
+    #Create dictionary of dictionary of the summed emotions for a cluster
+    for tweet in tweets:
+        emotions_dict = tweet['emotions']
+        current_centroid = centroids_dict.get(tweet['cluster'], {})
+
+        for emotion, value in tweet['emotions'].items():
+            emotion_count = current_centroid.get(emotion, 0)
+            current_centroid[emotion] = emotion_count + value
+        
+        centroids_dict[tweet['cluster']] = current_centroid
+
+        # Create dictionary with the number of elements in a cluster
+        if not tweet['cluster'] in cluster_memeber_count:
+            cluster_memeber_count[tweet['cluster']] = 0
+        
+        cluster_memeber_count[tweet['cluster']] += 1
+    
+    # Create centroids
+    for key, emotion in centroids_dict.items():
+        for em, value in emotion.items():
+            value = value/cluster_memeber_count[key]
+    
+    return centroids_dict
+
 def clustered_file_creation(tweets, file=''):
-    # Dump to a file
+    # Dump to a filec
     with open(file,"w") as file:
         file.write(json.dumps(tweets))
 
 if __name__ == '__main__':
     tweets = load_tweets("emotions.json")
     clustered_tweets = divisive_hierarhical_clustering(tweets)
+    centroids = create_centroids(clustered_tweets)
     clustered_file_creation(clustered_tweets, "clustered_tweets.json")
+    clustered_file_creation(centroids, "centroids_of_tweets.json")
