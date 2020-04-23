65998f11c31c4ccf7e7e664457e04a8dbc6e6627
andreea21ungureanu <andreea.ungureanu221@gmail.com>
2020-04-23 20:44:53 +0100

    investigation for clustering with silhouette

--- investigation/clustering.py
+++ investigation/clustering.py
@@ -2,9 +2,10 @@ import json
 from sklearn import metrics
 import os
 import sys
+from sklearn.cluster import DBSCAN, OPTICS
 
-sys.path.append('../src')
-from clustering import *
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from src.clustering import *
 
 def load_tweets(file=''):
     tweets = []
@@ -14,28 +15,99 @@ def load_tweets(file=''):
 
     return tweets
 
-def validate_clusters(tweets):
-    # Instantiate the vectors used for clustering
+def create_np_emotions_array_copy(tweets):
+    vectors = np.empty((0, 5))
+
+    # Add all the vectors to our matrix
+    for tweet in tweets:
+        emotions = tweet['emotions']
+        vector = np.array([[emotions['Excitement'],
+                            emotions['Anger'],
+                            emotions['Sadness'],
+                            emotions['Happiness'],
+                            emotions['Fear']]])
+        
+        vectors = np.append(vectors, vector, axis=0)
+
+    return vectors
+
+def create_np_emotions_array_kmeans(tweets):
+    vectors = np.empty((0, 5))
+
+    # Add all the vectors to our matrix
+    for tweet in tweets:
+        emotions = tweet['emotions']
+        vector = np.array([[emotions['Excited'],
+                            emotions['Angry'],
+                            emotions['Sad'],
+                            emotions['Happy'],
+                            emotions['Fear']]])
+        
+        vectors = np.append(vectors, vector, axis=0)
+
+    return vectors
+
+def dbscan_clustering(tweets):
     vectors = create_np_emotions_array(tweets)
 
-    # Instantiate clustering methods
-    divisive_hierarhical_clustering_v = divisive_hierarhical_clustering(tweets)
-    kmeans_clustering_v = kmeans_clustering(tweets)
+    # Cluster using DBSCAN
+    dbscan_clusters = DBSCAN(eps=0.07).fit_predict(vectors)
 
+    return dbscan_clusters
+
+def optics_clustering(tweets):
+    vectors = create_np_emotions_array(tweets)
+
+    # Cluster using DBSCAN
+    optics_clusters = OPTICS().fit_predict(vectors)
+
+    return optics_clusters
+   
+def recompute_clusters_from_divisive(tweets):
+    tweets_in_clusters = []
+    for tweet in tweets:
+        cluster_nr = tweet['cluster']
+        tweets_in_clusters.append(cluster_nr)
+    
+    return tweets_in_clusters
+
+def validate_clusters(tweets):
+
+    # Instantiate the vectors used for clustering
+    vectors = create_np_emotions_array_copy(tweets)
+
+    # Use the existent data about the clusters from the tweets file instead of recomputing the cluster number
+    reconstructed_divisive = recompute_clusters_from_divisive(tweets)
     # Validate the clustering methods
     # For Divisive Hierarhical Clustering
-    divisive_hierarhical_score = metrics.silhouette_score(vectors, divisive_hierarhical_clustering_v, metric='euclidean')
+    divisive_hierarhical_score = metrics.silhouette_score(vectors, reconstructed_divisive, metric='euclidean')
+
+    return divisive_hierarhical_score
+
+def investigate_kmeans(tweets):
+    vectors = create_np_emotions_array_kmeans(tweets)
+
+    # Instantiate clustering methods
+    kmeans_clustering_v = kmeans_clustering(tweets)
 
     # For KMeans Clustering
     kmeans_score = metrics.silhouette_score(vectors, kmeans_clustering_v, metric='euclidean')
 
-    if max(divisive_hierarhical_score, kmeans_score) == divisive_hierarhical_score:
-        print("DIVISIVE")
-    else:
-        print("KMEANS")
+    return kmeans_score
 
+def investigate_DBSCAN(tweets):
+    vectors = create_np_emotions_array_kmeans(tweets)
+
+    # Instantiate clustering methods
+    DBSCAN_clustering_v = dbscan_clustering(tweets)
+    print(DBSCAN_clustering_v)
+
+    DBSCAN_score = metrics.silhouette_score(vectors, DBSCAN_clustering_v)
+
+    return DBSCAN_score
 
 if __name__ == '__main__':
+    # tweets = load_tweets("./FlaskApp/perspectives_app/static/json/uklockdown/relabelled_clustered_tweets.json")
     tweets = load_tweets("./resources/emotions_collected/emotions_coronavirus.json")
-    validate_clusters(tweets)
-
+    # print(investigate_kmeans(tweets))
+    print(investigate_DBSCAN(tweets))
